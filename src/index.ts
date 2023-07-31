311d2bbe18567e340726aeda6b919bedf5e86010
Marek Lisy <marek.lisy.hk@gmail.com>
2023-08-01 00:23:49 +0200

    add to sync-all

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { clearDb, initDb } from "./db/db";
-import {
-  downloadAndImportAddressPoints,
-} from "./open-data-sync/address-points";
+import { downloadAndImportAddressPoints } from "./open-data-sync/address-points";
+import { importCities } from "./open-data-sync/cities";
 import { downloadAndImportRegions } from "./open-data-sync/regions";
 import { downloadAndImportSchools } from "./open-data-sync/schools";
 import { downloadAndImportStreets } from "./open-data-sync/streets";
 import { OpenDataSyncOptionsPartial } from "./utils/helpers";
 
-export * from "./street-markdown/types";
 export * from "./street-markdown/smd";
+export * from "./street-markdown/types";
 
 /**
  * Download and import all open data to SQLite DB. The DB file will be located
@@ -29,16 +28,18 @@ export async function downloadAndImportEverything(
   await downloadAndImportAddressPoints(options);
   await downloadAndImportSchools(options);
   await downloadAndImportRegions(options);
+  await importCities(options);
   if (syncStreets) {
     await downloadAndImportStreets(options);
   }
 }
 
 export {
-  initDb,
-  downloadAndImportAddressPoints,
   clearDb,
+  downloadAndImportAddressPoints,
+  downloadAndImportRegions,
   downloadAndImportSchools,
   downloadAndImportStreets,
-  downloadAndImportRegions,
+  importCities,
+  initDb,
 };
